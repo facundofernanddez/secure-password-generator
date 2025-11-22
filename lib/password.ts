bfa5fc14cbcc4c5a0b58e19b9b15eab590e9e54f
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2025-11-22 19:48:41 -0300

    create random password generator

--- lib/password.ts
+++ lib/password.ts
@@ -1,4 +1,4 @@
-interface Props {
+export interface PasswordConfig {
   hasLowercase?: boolean;
   hasUppercase?: boolean;
   hasNumbers?: boolean;
@@ -12,4 +12,27 @@ export const generatePassword = ({
   hasNumbers = false,
   hasSymbols = false,
   length = 8,
-}: Props) => {};
+}: PasswordConfig = {}) => {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  let availableChars = "";
+  if (hasLowercase) availableChars += lowercaseChars;
+  if (hasUppercase) availableChars += uppercaseChars;
+  if (hasNumbers) availableChars += numberChars;
+  if (hasSymbols) availableChars += symbolChars;
+
+  if (availableChars.length === 0) {
+    availableChars = lowercaseChars;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars[randomIndex];
+  }
+
+  return password;
+};
