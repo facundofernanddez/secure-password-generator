8f9bbf7a9224189ca62fb6c900516effbc423cc7
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2025-11-30 14:06:40 -0300

    create component for tags

--- app/(dashboard)/_components/password-options-tags.tsx
+++ app/(dashboard)/_components/password-options-tags.tsx
@@ -1,1 +1,37 @@
-export default function PasswordOptionsTags() {}
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { PasswordConfig } from "@/lib/password";
+
+interface Props {
+  passwordConfig: PasswordConfig;
+}
+
+export default function PasswordOptionsTags({ passwordConfig }: Props) {
+  return (
+    <div className="flex flex-wrap gap-2">
+      {[
+        {
+          condition: passwordConfig.hasLowercase,
+          label: "Minúsculas",
+        },
+        {
+          condition: passwordConfig.hasUppercase,
+          label: "Mayúsculas",
+        },
+        {
+          condition: passwordConfig.hasNumbers,
+          label: "Números",
+        },
+        {
+          condition: passwordConfig.hasSymbols,
+          label: "Símbolos",
+        },
+      ]
+        .filter((item) => item.condition)
+        .map((item, index) => (
+          <Badge key={index}>{item.label}</Badge>
+        ))}
+    </div>
+  );
+}
